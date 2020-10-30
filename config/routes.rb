2b8d1a2d1e5815bb8296ff85875128ea4529c6fe
harrypotter0045 <harrypotter0045@gmail.com>
2020-10-30 02:01:47 -0500

    About and Character View Add

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+  get "about/index"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  get "/characters", to: "character#index", as: "characters"
+
+  get "/character/:id", to: "character#show", as: "character"
 end
